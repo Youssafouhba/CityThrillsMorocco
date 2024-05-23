42468e9436640c05d34176a6acd49bb7662f5b47
Youssafouhba <youssefouhba@gmail.com>
2024-05-23 17:13:31 +0100

    admin_branch

---
+++ src/main/java/com/CityThrillsMorocco/config/IndexController.java
@@ -0,0 +1,12 @@
+package com.CityThrillsMorocco.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping(value = "/{[path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/index.html";
+    }
+}
--- src/main/java/com/CityThrillsMorocco/config/MvcConfig.java
+++ src/main/java/com/CityThrillsMorocco/config/MvcConfig.java
@@ -14,16 +14,6 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/src/main/resources/templates/")
-                .resourceChain(true)
-                .addResolver(new PathResourceResolver() {
-                    @Override
-                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
-                        Resource requestedResource = location.createRelative(resourcePath);
-
-                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-                                : new ClassPathResource("/templates/index.html");
-                    }
-                });
+                .addResourceLocations("classpath:/static/");
     }
 }
