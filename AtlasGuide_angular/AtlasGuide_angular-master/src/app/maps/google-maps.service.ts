28048f99487c93f49a5168e7be56f6504e193f67
Youssafouhba <youssefouhba@gmail.com>
2024-05-23 21:57:31 +0100

    admin_branch

--- AtlasGuide_angular/AtlasGuide_angular-master/src/app/maps/google-maps.service.ts
+++ AtlasGuide_angular/AtlasGuide_angular-master/src/app/maps/google-maps.service.ts
@@ -1,7 +1,6 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import axios from 'axios'
 import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
-import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
